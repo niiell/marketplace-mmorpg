6ae5655c5017c995727ddbd38fb7e2ce174c4fff
niiell <macengbot@gmail.com>
2025-05-06 11:07:54 +0700

    Test

--- content/landing.md
+++
@@ -1,1 +0,0 @@
-title: Welcome to Marketplace MMORPG SEA description: "This platform offers secure and fast transactions of MMORPG items, gold, and services across Southeast Asia." type: Page
--- src/app/listing/new/NewListingClient.tsx
+++ src/app/listing/new/NewListingClient.tsx
@@ -3,6 +3,7 @@
 import { useState, useEffect } from "react";
 import { supabase } from '../../../lib/supabase';
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 export default function NewListing() {
   const [title, setTitle] = useState("");
